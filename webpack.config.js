0c3a665c35d0c60fa930cdd8f8db7c5b86f07bb4
xion <xiongxiao1012@outlook.com>
2022-03-22 10:38:36 +0800

    暂存

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WEBPACK_SERVE = process.env.WEBPACK_SERVE;
 const join = path.join;
 const mode = WEBPACK_SERVE ? 'development' : 'production';
+const { ModuleFederationPlugin } = require('webpack').container;
+
 
 /** @type {import('webpack').Configuration} */
 const common = {
@@ -104,6 +106,31 @@ const common = {
       cache: true,
       version: pkgs.version,
     }),
+    new ModuleFederationPlugin({
+      filename: 'remoteEntry.js',
+      remotes: {
+        // 可以将其他项目的 name 映射到当前项目中
+        // component: 'component@../component/js/remoteEntry.js',
+        // "component": "component@http://localhost:3002/remoteEntry.js",
+        // "admin": "admin@http://localhost:3001/remoteEntry.js",
+      },
+      exposes: {
+        // 导出的模块，只有在此申明的模块才可以作为远程依赖被使用。
+        './App': './src/App',
+      },
+      shared: {
+        // 是非常重要的参数，制定了这个参数，可以让远程加载的模块对应依赖改为使用本地项目的 React 或 ReactDOM
+        react: pkgs.dependencies['react'],
+        lodash: pkgs.dependencies['lodash'],
+        dayjs: pkgs.dependencies['dayjs'],
+        'react-dom': pkgs.dependencies['react-dom'],
+        'react-router-dom': pkgs.dependencies['react-router-dom'],
+        // ahooks: '^2.10.9',
+        // antd: {
+        //   version: '^4.16.12',
+        // },
+      }
+    })
   ],
 };
 
